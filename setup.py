831a3007590b6a5f969cc3457fffa89c0c2bd4f2
Jacek Tomaszewski <jacek.tomek@gmail.com>
2012-05-23 15:40:24 +0200

    Update setup.py info and version.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ajax-validation',
-    version='0.1.6',
-    description='This is fork of original Alex Gaynor\'s django-ajax-validation.',
-    author='Kuba Janoszek',
-    author_email='kuba.janoszek@gmail.com',
-    url='http://github.com/jqb/django-ajax-validation/tree/master',
+    version='0.1.6-pozytywnie-1',
+    description="This is fork of Kuba Janoszek's fork of original Alex Gaynor's django-ajax-validation.",
+    author='Jacek Tomaszewski',
+    author_email='jacek.tomek@gmail.com',
+    url='http://github.com/zlorf/django-ajax-validation/tree/master',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
